10890765c3e522ddbbd3c08d725eba874aa1c351
Victorparo <victor.paro@viaconsulting.com.br>
2024-06-18 16:25:44 -0300

    adjusting password

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useContext } from 'react';
 import axios from 'axios';
+import { error } from 'console';
 
 interface IAuthContext {
     logged: boolean;
@@ -18,19 +19,31 @@ const AuthProvider: React.FC = ({ children }) => {
 
     const signIn = async (email: string, password: string) => {
         try {
+
+            const email2 = `${email}`
+            console.log(email2)
+            const senha2 = `${password}`
+            console.log(senha2)
+
             const response = await axios.post('http://localhost:8080/usuarios/login', {
-                email,
-                senha: password
+                email: email2,
+                senha: senha2
             });
 
-            if (response.data.success) {
+            console.log("resposta api --> ",response)
+
+            if (response.status = 200) {
                 localStorage.setItem('@minha-carteira:logged', 'true');
                 setLogged(true);
+                console.log(email)
+                console.log(password)
             } else {
                 alert('Senha ou usuário inválidos!');
             }
         } catch (error) {
             alert('Erro ao fazer login! Por favor, tente novamente.');
+            console.log(email)
+            console.log(password)
         }
     }
 
